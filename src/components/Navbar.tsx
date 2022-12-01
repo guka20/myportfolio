d9867942bdbd4652aff9d258fbd74999ac65ebc8
guka20 <61795263+guka20@users.noreply.github.com>
2022-12-01 22:43:03 +0400

    done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,38 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { GiHamburgerMenu } from "react-icons/gi";
 export const Navbar = () => {
   const [isActive, setIsActive] = useState<boolean>(false);
+  const [isUsed, setIsUsed] = useState<boolean>(false);
   return (
-    <nav className="nav_bar">
-      <div className="nav_bar_border">
-        <div className="logo">My Portfolio</div>
-        <button className="hamburger" onClick={() => setIsActive(!isActive)}>
-          <GiHamburgerMenu />
-        </button>
-        <ul className={isActive ? "navigators active" : "navigators"}>
-          <Link to="/">
-            <li>Home</li>
-          </Link>
-          <Link to="Projects">
-            <li>Projects</li>
-          </Link>
-          <Link to="/Contact">
-            <li>Contact</li>
-          </Link>
+    <nav className="navbar">
+      <div className="nav">
+        <h1>My Portfolio</h1>
+        <ul id="links" className={isActive ? "animate" : isUsed ? "close" : ""}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
+        <button
+          className="hamburger"
+          onClick={() => {
+            setIsActive(!isActive);
+            setIsUsed(true);
+          }}
+          onBlur={() => {
+            setIsActive(false);
+            setIsUsed(true);
+          }}
+        >
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </button>
       </div>
     </nav>
   );
